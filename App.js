6504b3acd019ded3753f2025920ee39b41397da0
EltonZierhut <eltonzierhut@gmail.com>
2024-08-26 21:17:35 -0300

    add files

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import StackNavigator from './src/navigation/StackNavigator';
+import DrawerNavigator from './src/navigation/DrawerNavigator';
+
+export default function App() {
+  // return <StackNavigator />;
+  return <DrawerNavigator />;
+}
